fac8bb0cb0525d26638f102af742534ad5e32a01
SammyBits <samuelbeato7@gmail.com>
2025-09-14 00:11:56 -0400

    feat(rate-limiter): refactor rate limiting configuration for public and sensitive endpoints

--- src/app.ts
+++ src/app.ts
@@ -6,24 +6,34 @@ import { logger } from "hono/logger";
 import { AuthRouter } from "@modules/Auth/interfaces/router";
 import { ProfileRouter } from "@modules/Profile/interfaces/ProfileRouter";
 import { jwt, type JwtVariables } from "hono/jwt";
+import { secureHeaders } from "hono/secure-headers";
 
 type Variables = JwtVariables;
 
 const app = new Hono<{ Variables: Variables }>();
 app.use("*", logger());
-// app.use(
-//   "/profile/*",
-//   jwt({
-//     secret: Bun.env.JWT_SECRET,
-//   })
-// );
+app.use(
+  "*",
+  secureHeaders({
+    strictTransportSecurity: "max-age=63072000; includeSubDomains; preload",
+    xFrameOptions: false,
+    xXssProtection: "1; mode=block",
+    xContentTypeOptions: false,
+    contentSecurityPolicy: {
+      scriptSrc: ["nonce"],
+    },
+    referrerPolicy: "strict-origin-when-cross-origin",
+    permissionsPolicy: {
+      geolocation: false,
+    },
+  })
+);
 app.route("/api/v1/auth", AuthRouter);
 app.route("/api/v1/profile", ProfileRouter);
 
 app.notFound((c) => c.json({ message: "Not Found" }, 404));
 
 app.onError((err, c) => {
-  // Querio los de stack, quiero saber ue pasa
   console.error("Stack trace:", err.stack);
   console.error(`Error occurred: ${err}`);
   return c.json({ message: "Internal Server Error" }, 500);
